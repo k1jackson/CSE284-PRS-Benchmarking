876fe50e4be52c918d9910b46e12ce1ab70a626a
k1jackson <k8jackson@k8jackson>
2026-03-04 14:27:06 -0800

    Computed PRS for 1000Genomes data

--- LDPred2/LDPred2_running.R
+++ LDPred2/LDPred2_running.R
@@ -3,8 +3,9 @@ library(bigreadr)
 library(tidyverse)
 library(data.table)
 
-# load Harvard PGP data
-rds_path <- snp_readBed("PGP_filtered_data.bed") #"clean_plink_data.bed")
+# Load Harvard PGP or 1000 Genomes data
+rds_path <- snp_readBed("PGP_filtered_data.bed") 
+# rds_path <- snp_readBed("1000Genomes/1000G_merged_pruned.bed") 
 obj.bigSNP <- snp_attach(rds_path)
 genotypes <- snp_fastImputeSimple(obj.bigSNP$genotypes, method = "mean0", ncores = nb_cores())
 SNPs <- obj.bigSNP$map
@@ -12,26 +13,27 @@ names(SNPs) <- c("chr", "rsid", "dist", "pos", "a1", "a0")
 fam.order <- as.data.table(obj.bigSNP$fam)
 setnames(fam.order, c("family.ID", "sample.ID"), c("FID", "IID"))
 
-# load summary statistics
+# Load summary statistics
 sum_stats <- fread2("GIANT_GWAS_summary_stats.tsv")
 names(sum_stats) <- c("id", "rsid", "chr", "pos", "a1", "a0", "EAF", "beta", "beta_se", "p", "n_eff")
 sum_stats <- sum_stats %>% filter(chr %in% 1:22)
 
-# filter SNPs
+# Filter SNPs
 hapmap3 <- readRDS("map_hm3_plus.rds")
 info_SNPs <- snp_match(sum_stats, hapmap3, join_by_pos = TRUE)
 setnames(info_SNPs, old = "_NUM_ID_", new = "hapmap3_index")
 info_SNPs <- snp_match(info_SNPs, SNPs, join_by_pos = FALSE, match.min.prop = 0)
 setnames(info_SNPs, old = "_NUM_ID_", new = "plink_index")
 
-# save data for PRSice
-# fwrite2(info_SNPs %>% select("rsid", "chr", "pos", "a1", "a0", "beta", "p"), "PRSice_sumstats.txt", sep = "\t", na = "NA")
-# obj.bigSNP$genotypes <- genotypes
-# rds_path_subset <- subset(obj.bigSNP, ind.col = info_SNPs$plink_index)
-# obj.bigSNP.subset <- snp_attach(rds_path_subset)
-# snp_writeBed(obj.bigSNP.subset, bedfile = "PRSice_PGP_data.bed")
+# Save filtered and inputed data for PRSice
+fwrite2(info_SNPs %>% select("rsid", "chr", "pos", "a1", "a0", "beta", "p"), "PRSice_sumstats.txt", sep = "\t", na = "NA")
+obj.bigSNP$genotypes <- genotypes
+rds_path_subset <- subset(obj.bigSNP, ind.col = info_SNPs$plink_index)
+obj.bigSNP.subset <- snp_attach(rds_path_subset)
+snp_writeBed(obj.bigSNP.subset, bedfile = "PRSice_PGP_data.bed")
+# snp_writeBed(obj.bigSNP.subset, bedfile = "PRSice_1000G_data.bed")
 
-# construct LD matrix
+# Find correlations from precomputed LD matrix
 SNPs_pos <- snp_asGeneticPos(info_SNPs$chr, info_SNPs$pos, dir = ".")
 tmp <- tempfile(tmpdir = "tmp-data")
 on.exit(file.remove(paste0(tmp, ".sbk")), add = TRUE)
@@ -57,7 +59,7 @@ df_beta <- info_SNPs[,c("beta", "beta_se", "n_eff", "hapmap3_index")]
 ldsc <- snp_ldsc(ld, length(ld), chi2 = (df_beta$beta / df_beta$beta_se)^2, sample_size = df_beta$n_eff, blocks = NULL)
 h2_est <- ldsc[["h2"]]
 
-# compute PRS
+# Compute PRS
 final_beta <- snp_ldpred2_inf(corr, info_SNPs, h2 = h2_est)
 prs <- big_prodVec(genotypes, final_beta, ind.row = 1:nrow(genotypes), ind.col = info_SNPs$plink_index)
 results <- data.frame(sample = sub("_genome", "", fam.order$FID), PRS = prs)
--- PRSice2/PRSice2_running.sh
+++ PRSice2/PRSice2_running.sh
@@ -16,3 +16,18 @@ Rscript PRSice.R \
     --cov PRSice_phenotypes.txt \
     --cov-col sex_num \
     --out PRSice_PRS
+
+Rscript PRSice.R \
+    --prsice ./PRSice_linux \
+    --base 1000Genomes/PRSice_sumstats.txt \
+    --target 1000Genomes/PRSice_1000G_data \
+    --snp rsid \
+    --chr chr \
+    --bp pos \
+    --A1 a1 \
+    --A2 a0 \
+    --stat beta \
+    --pvalue p \
+    --no-regress \
+    --bar-levels 1 \
+    --out PRSice_1000G
